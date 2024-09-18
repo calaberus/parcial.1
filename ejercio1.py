646edd16bf43341a6fe90363db3701f108733fff
calaberus <juan.esteban.mondragon@correounivalle.edu.co>
2024-09-18 10:00:38 -0500

    Add files via upload
    Respuesta: #numeros pedidos a los usuarios
    numero1=int(input("ingrese un numero:"))
    numero2=int(input("ingrese un numero:"))
    
    #operaciones basicas
    suma=numero1+numero2
    resta=numero1-numero2
    multiplicacion=numero1*numero2
    division=numero1/numero2
    
    #resultados
    print(f"la suma fue:{suma},la resta fue:{resta},la multiplicacion fue:{multiplicacion},la divison fue:{division}")

---
+++ ejercio1.py
@@ -0,0 +1,12 @@
+Respuesta: #numeros pedidos a los usuarios
+numero1=int(input("ingrese un numero:"))
+numero2=int(input("ingrese un numero:"))
+
+#operaciones basicas
+suma=numero1+numero2
+resta=numero1-numero2
+multiplicacion=numero1*numero2
+division=numero1/numero2
+
+#resultados
+print(f"la suma fue:{suma},la resta fue:{resta},la multiplicacion fue:{multiplicacion},la divison fue:{division}")
